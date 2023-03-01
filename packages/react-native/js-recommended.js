80db22fae8f0eaa8fc5b088adf5e732862d1144d
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2023-03-01 17:21:07 -0300

    Define ReactJS config

---
+++ packages/react-native/js-recommended.js
@@ -0,0 +1,84 @@
+module.exports = {
+  env: {
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [
+    {
+      files: ['*.json'],
+      rules: {
+        'no-unused-expressions': 'off',
+        'prettier/prettier': [
+          'error',
+          {
+            printWidth: 80,
+            singleQuote: false,
+            tabs: false,
+            tabWidth: 4,
+            trailingComma: 'none',
+          },
+        ],
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 14,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks', 'prettier'],
+  rules: {
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      { js: 'never', jsx: 'never' },
+    ],
+    'import/order': [
+      'error',
+      {
+        alphabetize: { order: 'asc' },
+        pathGroups: [
+          {
+            pattern:
+              '{react,react-native,styled-components,styled-components/native}',
+            group: 'external',
+            position: 'before',
+          },
+        ],
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          ['index', 'sibling', 'parent'],
+          'object',
+          'type',
+        ],
+        'newlines-between': 'always',
+        pathGroupsExcludedImportTypes: ['react'],
+        warnOnUnassignedImports: true,
+      },
+    ],
+    'import/prefer-default-export': 'off',
+    'no-console': 'error',
+    'prettier/prettier': [
+      'error',
+      {
+        arrowParens: 'always',
+        printWidth: 80,
+        singleQuote: true,
+        tabs: false,
+        tabWidth: 2,
+        trailingComma: 'all',
+      },
+    ],
+    'react-hooks/exhaustive-deps': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+    'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
+  },
+};
---
+++ packages/react-native/ts-recommended.js
@@ -0,0 +1,121 @@
+module.exports = {
+  env: {
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [
+    {
+      files: ['*.json'],
+      rules: {
+        'no-unused-expressions': 'off',
+        '@typescript-eslint/no-unused-expressions': 'off',
+        'prettier/prettier': [
+          'error',
+          {
+            printWidth: 80,
+            singleQuote: false,
+            tabs: false,
+            tabWidth: 4,
+            trailingComma: 'none',
+          },
+        ],
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 14,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
+  rules: {
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'interface',
+        format: ['PascalCase'],
+        custom: {
+          regex: '^I[A-Z]',
+          match: true,
+        },
+      },
+      {
+        selector: ['typeAlias', 'typeParameter'],
+        format: ['PascalCase'],
+        custom: {
+          regex: '^T[A-Z]',
+          match: true,
+        },
+      },
+    ],
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-use-before-define': ['error'],
+    '@typescript-eslint/no-useless-constructor': 'warn',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      { ts: 'never', tsx: 'never' },
+    ],
+    'import/order': [
+      'error',
+      {
+        alphabetize: { order: 'asc' },
+        pathGroups: [
+          {
+            pattern:
+              '{react,react-native,styled-components,styled-components/native}',
+            group: 'external',
+            position: 'before',
+          },
+        ],
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          ['index', 'sibling', 'parent'],
+          'object',
+          'type',
+        ],
+        'newlines-between': 'always',
+        pathGroupsExcludedImportTypes: ['react'],
+        warnOnUnassignedImports: true,
+      },
+    ],
+    'import/prefer-default-export': 'off',
+    'no-console': 'error',
+    'no-use-before-define': 'off',
+    'no-useless-constructor': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        arrowParens: 'always',
+        printWidth: 80,
+        singleQuote: true,
+        tabs: false,
+        tabWidth: 2,
+        trailingComma: 'all',
+      },
+    ],
+    'react-hooks/exhaustive-deps': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+    'react/jsx-filename-extension': ['warn', { extensions: ['.tsx'] }],
+    'react/jsx-props-no-spreading': 'off',
+    'react/react-in-jsx-scope': 'off',
+  },
+  settings: {
+    'import/parsers': {
+      [require.resolve('@typescript-eslint/parser')]: ['.ts', '.tsx', '.d.ts'],
+    },
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+};
